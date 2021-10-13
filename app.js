c270d0e28068db198d27f2ec46069181d20d808f
Chris <chrischakchay22@yahoo.com>
2021-10-12 23:09:07 -0500

    added winning score select

--- app.js
+++ app.js
@@ -2,13 +2,54 @@ const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
 const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
+const resetButton = document.querySelector('#reset');
+const winningScoreSelect = document.querySelector('#playUpTo');
+
 
 let p1Score = 0;
-let winningScore = 5;
+let p2Score = 0;
+let winningScore = 3;
+let gameOver = false;
 
-p1Button.addEventListener('click', function() {
-    if( p1Score !== winningScore){
-    p1Score += 1;
-    p1Display.textContent = p1Score
+p1Button.addEventListener('click', function () {
+    if (!gameOver) {
+        p1Score += 1;
+        if (p1Score === winningScore) {
+            gameOver = true;
+            p1Display.classList.add('winner');
+            p2Display.classList.add('loser');
+        }
+        p1Display.textContent = p1Score
     }
 })
+
+p2Button.addEventListener('click', function () {
+    if (!gameOver) {
+        p2Score += 1;
+        if (p2Score === winningScore) {
+            gameOver = true;
+            p2Display.classList.add('winner');
+            p1Display.classList.add('loser');
+        }
+        p2Display.textContent = p2Score
+    }
+})
+
+//winning score select dropdown
+winningScoreSelect.addEventListener('change', function () {
+    winningScore = parseInt(this.value)
+    reset();
+})
+
+//reset scoreboard
+resetButton.addEventListener('click', reset);
+
+function reset() {
+    gameOver = false;
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.classList.remove('winner', 'loser');
+    p2Display.classList.remove('winner', 'loser');
+}
